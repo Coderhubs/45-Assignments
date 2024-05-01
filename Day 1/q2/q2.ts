295159cf4a25fe771474318f4a7d1d41990ed997
Sumra Jabeen <160333925+Coderhubs@users.noreply.github.com>
2024-05-01 20:19:55 +0500

    version 1

---
+++ Day 1/q2/q2.ts
@@ -0,0 +1,9 @@
+// DAY 1 CHALLENGE
+
+// Question # 02
+
+// Personal Message: Store a person’s name in a variable and print a message to them, 
+//like “Hello Asharib, would you like to learn some TypeScript today?”
+
+let Personname: string = ("Hello Simra,would u like to learn some Typescript today?");
+console.log("Hello simra,would u like to learn some Typescript today?");
